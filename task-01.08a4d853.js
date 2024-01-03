d7b28b010e9b9e6d50fc17f29702630824a75c0f
Afiron09 <136248287+Afiron09@users.noreply.github.com>
2024-01-03 16:14:50 +0000

    Deploying to gh-pages from @ Afiron09/1@9fdc5b0be2a6b1544291a885c9d3ceba6be2fbfb 🚀

---
+++ task-01.08a4d853.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories");console.log(`Number of categories: ${e.children.length}`),[...e.children].forEach((e=>{console.log(e.firstElementChild.textContent),console.log(e.lastElementChild.children.length)}));
+//# sourceMappingURL=task-01.08a4d853.js.map
---
+++ task-02.cca31c26.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#ingredients"),t=["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments"].map((e=>{const t=document.createElement("li");return t.textContent=e,t.classList="item",t}));e.append(...t);
+//# sourceMappingURL=task-02.cca31c26.js.map
